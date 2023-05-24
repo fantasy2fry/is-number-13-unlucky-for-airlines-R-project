4f9297f2eb80f009dbaba928a6fcd40d7045056f
Norbert <01180700@pw.edu.pl>
2023-05-24 11:26:22 +0200

    Zrobilem to co ustalilismy we wtorek wieczorem

--- test1.R
+++ test1.R
@@ -1,17 +1,18 @@
 options(browser = "chromium") # inaczej mi shiny nie działało
-library(data.table)
-test<-as.data.table( read.csv("~/Dokumenty/informatyczne/iadstudia/pdu/pd4/2007.csv.bz2"))
-test2<-as.data.table( read.csv("~/Dokumenty/informatyczne/iadstudia/pdu/pd4/2003.csv.bz2"))
-test<-as.data.table( read.csv("~/Dokumenty/informatyczne/iadstudia/pdu/pd4/2008.csv.bz2"))
-library(sqldf)
-sqldf("SELECT * FROM test 
-      WHERE TailNum LIKE '%13%'")-> test2
-library(dplyr)
-rm(test) #usuwanie zmiennych :)
-
-result <- test %>%
-  filter(grepl("13", FlightNum)) %>%
-  select_if(~!any(is.na(.)))
+# library(data.table)
+# #od '95 jest TailNum
+# test<-read.csv("~/Dokumenty/informatyczne/iadstudia/pdu/pd4/1994.csv.bz2")
+# test2<-as.data.table( read.csv("~/Dokumenty/informatyczne/iadstudia/pdu/pd4/2003.csv.bz2"))
+# test<-as.data.table( read.csv("~/Dokumenty/informatyczne/iadstudia/pdu/pd4/2008.csv.bz2"))
+# library(sqldf)
+# sqldf("SELECT * FROM test
+#       WHERE TailNum LIKE '%13%'")-> test2
+# library(dplyr)
+# rm(test) #usuwanie zmiennych :)
+# 
+# result <- test %>%
+#   filter(grepl("13", FlightNum)) %>%
+#   select_if(~!any(is.na(.)))
 
 # 'result' will contain the filtered data frame with columns having no NA values
 
@@ -36,17 +37,53 @@ odwolania <- function(czyZ13, column){
     }else{
       wynik=rbind(wynik,test2)
   }
-  
   rm(tabela)
   }
+  dodatkowa_kolumna = as.data.frame(wynik[,1]/wynik[,2])*100
+  colnames(dodatkowa_kolumna) = "[,1]/[,2] %"
+  wynik=cbind(wynik, dodatkowa_kolumna)
   return(wynik)
 }
 
 wynik_odwolania_z_13_w_TailNum = odwolania(TRUE, "TailNum")
 
 wynik_odwolania_z_13_w_FlightNum = odwolania(TRUE, "FlightNum")
-library(ggplot2) # moze
 
-dodatkowa_kolumna = as.data.frame(wynik_odwolania_z_13[,1]/wynik_odwolania_z_13[,2])*100
-colnames(dodatkowa_kolumna) = "[,1]/[,2] %"
-wynik_prim=cbind(wynik_odwolania_z_13, dodatkowa_kolumna)
+wynik_odwolania_z_13_w_Day = odwolania(TRUE, "DayofMonth")
+
+wynik_odwolania_bez_13_w_TailNum = odwolania(FALSE, "TailNum")
+
+wynik_odwolania_bez_13_w_FlightNum = odwolania(FALSE, "FlightNum")
+
+wynik_odwolania_bez_13_w_Day = odwolania(FALSE, "DayofMonth")
+
+write.csv(wynik_odwolania_z_13_w_Day, 
+          file="~/Dokumenty/informatyczne/iadstudia/pdu/pd4/tabelki/odwolania_z_13_Day.csv")
+
+write.csv(wynik_odwolania_z_13_w_TailNum, 
+          file="~/Dokumenty/informatyczne/iadstudia/pdu/pd4/tabelki/odwolania_z_13_TailNum.csv")
+write.csv(wynik_odwolania_z_13_w_FlightNum, 
+          file="~/Dokumenty/informatyczne/iadstudia/pdu/pd4/tabelki/odwolania_z_13_FlightNum.csv")
+write.csv(wynik_odwolania_bez_13_w_Day, 
+          file="~/Dokumenty/informatyczne/iadstudia/pdu/pd4/tabelki/odwolania_bez_13_Day.csv")
+write.csv(wynik_odwolania_bez_13_w_TailNum, 
+          file="~/Dokumenty/informatyczne/iadstudia/pdu/pd4/tabelki/odwolania_bez_13_TailNum.csv")
+write.csv(wynik_odwolania_bez_13_w_FlightNum, 
+          file="~/Dokumenty/informatyczne/iadstudia/pdu/pd4/tabelki/odwolania_bez_13_FlightNum.csv")
+
+# library(ggplot2) # moze
+
+# dodatkowa_kolumna = as.data.frame(wynik_odwolania_z_13[,1]/wynik_odwolania_z_13[,2])*100
+# colnames(dodatkowa_kolumna) = "[,1]/[,2] %"
+# wynik_prim=cbind(wynik_odwolania_z_13, dodatkowa_kolumna)
+
+# hist(wynik_odwolania_z_13_w_TailNum[[1]])
+# 
+# test = read.csv("~/Dokumenty/informatyczne/iadstudia/pdu/pd4/carriers.csv")
+# test = read.csv("~/Dokumenty/informatyczne/iadstudia/pdu/pd4/plane-data.csv")
+# test = read.csv("~/Dokumenty/informatyczne/iadstudia/pdu/pd4/variable-descriptions.csv")
+# test = read.csv("~/Dokumenty/informatyczne/iadstudia/pdu/pd4/airports.csv")
+
+# 1. wygenerowac troche tabel i zapisac do pliku
+# 2. przygotowac pliki z tabelka dla Pani Dominiki
+# 3. przygotuwac pliczek R zeby tylko wrzucila swoje sciezki
